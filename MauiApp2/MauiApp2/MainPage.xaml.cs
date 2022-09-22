d9d91be5a745dd11ceddb308c50c83fd33e17584
ItzBob <51436202+SvenN94@users.noreply.github.com>
2022-09-22 12:08:37 +0200

    Leet translator teogevoegd oefening 4 deel 1

---
+++ MauiApp2/MauiApp2/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿namespace MauiApp2;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnEntryTextChanged(object sender, TextChangedEventArgs e)
+	{
+
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+
+	}
+}
+
